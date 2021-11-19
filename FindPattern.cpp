963683df4aca76eb4fb8c346bd3958aa0a1f462e
developer <pavlov.as@phystech.edu>
2021-11-19 10:02:25 +0300

    Add FindPattern's class

--- FindPattern.cpp
+++ FindPattern.cpp
@@ -1,2 +1,20 @@
 #include "FindPattern.h"
+#include <utility>
+#include <ostream>
 
+// конструктор с параметрами с использованием move-семантики
+FindPattern::FindPattern(std::string fileName, size_t lineNum, std::string line) :
+    fileName(std::move(fileName)), lineNum(static_cast<int>(lineNum)), line(std::move(line)) {}
+
+// переопределяем оператор вывода
+std::ostream &operator<<(std::ostream & out, const FindPattern &result) {
+   if (result.lineNum == -1) {
+       out << "Sorry, but this pattern doesn't exist in such directory.\n";
+       return out;
+   }
+
+   out << "File's name is " << result.fileName << "\n";
+   out << "Line's number is " << result.lineNum << "\n";
+   out << "Line is " << result.line << "\n";
+   return out;
+}
--- FindPattern.h
+++ FindPattern.h
@@ -1,10 +1,23 @@
 #ifndef PARALLEL_SEARCH_FINDPATTERN_H
 #define PARALLEL_SEARCH_FINDPATTERN_H
 
+#include <string>
 
+/*
+ * Полученный результат - имя файла, номер строки, в которой был найден pattern, сама искомая строка
+ */
 class FindPattern {
+public:
+    const std::string fileName;                                              // имя файла
+    const int lineNum = -1;                                                  // номер искомой строки
+    const std::string line;                                                  // искомая строка
 
+    FindPattern() = default;
+    FindPattern(std::string, size_t, std::string);
+
+    friend std::ostream& operator<< (std::ostream&, const FindPattern&);   // переопределенный оператор вывода
 };
+
 
 
 #endif //PARALLEL_SEARCH_FINDPATTERN_H
